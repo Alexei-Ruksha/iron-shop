6563da530aae824733e4948b46c4fcebda201ef2
Alexei Ruksha <alexei.ruksha@intexsoft.by>
2018-03-11 09:50:22 +0300

    simple version of product navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './service/product.service';
+import { ImagesService } from './service/images.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Iron shop';
+
+  selectedChapter: any;
+  selectedProducts: any;
+  public chapters;
+  public order = {};
+
+  constructor(private productService: ProductService, private imageService: ImagesService) {
+  }
+
+
+  ngOnInit(): void {
+    this.chapters = this.productService.getChapters();
+    for (let chapter of this.chapters) {
+      this.order[chapter.id] = {};
+    }
+  }
+
+  public onSelectChapter(chapter) {
+    this.selectedChapter = chapter;
+    this.selectedProducts = this.productService.getProducts(chapter.id);
+  }
+
+  public decreaseProduct(productId, index) {
+    if (this.order[this.selectedChapter.id][productId] && this.order[this.selectedChapter.id][productId] > 1) {
+      this.order[this.selectedChapter.id][productId]--;
+    } else {
+      this.order[this.selectedChapter.id][productId] = null;
+    }
+  }
+
+  public increaseProduct(productId, index) {
+    if (this.order[this.selectedChapter.id][productId]) {
+      this.order[this.selectedChapter.id][productId]++;
+    } else {
+      this.order[this.selectedChapter.id][productId] = 1;
+    }
+  }
 }
